b86bb546211fbc3b161b1868b93b2379da697dff
Anshul Jagota <anshul.jagota@yahoo.com>
2025-05-10 15:22:26 +0530

    Update bank_account.go

--- exercises/practice/bank-account/bank_account.go
+++ exercises/practice/bank-account/bank_account.go
@@ -1,19 +1,49 @@
 package account
+import (
+"sync"	// makes it thread- safe 
+)
 
 // Define the Account type here.
 
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount <0 {
+	return nil
+return &Account{balance: amount}
 }
+        }
 
 func (a *Account) Balance() (int64, bool) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.closed   
+        {
+	return 0,false	
+	}
+	return a.balance,true
 	panic("Please implement the Balance function")
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	a.mu.Lock(){
+		defer a.mu.Unlock()
+	if a.closed|| a.balance+amount<0 
+                {
+			return 0, false
+                }
+a.balance+=amount
+return a.balance, true
+			
 }
 
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.close
+        {
+		return 0, false
+	}
+	a.closed = true
+	payout:=a.balance
+	return payout, true
+	
 }
