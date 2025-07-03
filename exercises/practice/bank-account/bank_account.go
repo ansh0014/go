a8d32f41bd103769a8c4bfdebc90ce2caa273099
ansh0014 <anshul.jagota@yahoo.com>
2025-07-03 13:01:53 +0530

    Fixes for bank account exercise Defined Account struct and implemented the methods
    Signed-off-by: ansh0014 <anshul.jagota@yahoo.com>

--- exercises/practice/bank-account/bank_account.go
+++ exercises/practice/bank-account/bank_account.go
@@ -1,19 +1,62 @@
 package account
 
-// Define the Account type here.
+import "sync"
+
+type Account struct {
+	mu      sync.Mutex
+	balance int64
+	open    bool
+}
 
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount < 0 {
+		return nil
+	}
+	return &Account{
+		balance: amount,
+		open:    true,
+	}
 }
 
 func (a *Account) Balance() (int64, bool) {
-	panic("Please implement the Balance function")
+	if a == nil {
+		return 0, false
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.open {
+		return 0, false
+	}
+	return a.balance, true
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	if a == nil {
+		return 0, false
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.open {
+		return 0, false
+	}
+	if a.balance+amount < 0 {
+		return a.balance, false
+	}
+	a.balance += amount
+	return a.balance, true
 }
 
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	if a == nil {
+		return 0, false
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if !a.open {
+		return 0, false
+	}
+	payout := a.balance
+	a.balance = 0
+	a.open = false
+	return payout, true
 }
